4ae806c1492166600636c46dca9d9f24f0733cb1
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2024-10-22 01:54:32 +0300

    added patterns

--- src/main/java/edu/nadn2tetris/tokenizer/Keyword.java
+++ src/main/java/edu/nadn2tetris/tokenizer/Keyword.java
@@ -1,21 +1,56 @@
 package edu.nadn2tetris.tokenizer;
 
+import java.util.regex.Pattern;
+
 public enum Keyword {
-    CLASS,
-    METHOD,
-    FUNCTION,
-    CONSTRUCTOR,
-    INT,
-    BOOLEAN,
-    FIELD,
-    LET,
-    DO,
-    IF,
-    ELSE,
-    WHILE,
-    RETURN,
-    TRUE,
-    FALSE,
-    NULL,
-    THIS
+    CLASS(Pattern.compile("^class$")),
+    CONSTRUCTOR(Pattern.compile("^constructor$")),
+    FUNCTION(Pattern.compile("^function$")),
+    METHOD(Pattern.compile("^method$")),
+    FIELD(Pattern.compile("^field$")),
+    STATIC(Pattern.compile("^static$")),
+    VAR(Pattern.compile("^var$")),
+    INT(Pattern.compile("^int$")),
+    CHAR(Pattern.compile("^char$")),
+
+    BOOLEAN(Pattern.compile("^boolean$")),
+    VOID(Pattern.compile("^void$")),
+    TRUE(Pattern.compile("^true$")),
+    FALSE(Pattern.compile("^false$")),
+    NULL(Pattern.compile("^null$")),
+    THIS(Pattern.compile("^this$")),
+    LET(Pattern.compile("^let$")),
+    DO(Pattern.compile("^do$")),
+    IF(Pattern.compile("^if$")),
+    ELSE(Pattern.compile("^else$")),
+    WHILE(Pattern.compile("^while$")),
+    RETURN(Pattern.compile("^return$"));
+
+    private final Pattern pattern;
+
+    Keyword(Pattern pattern) {
+        this.pattern = pattern;
+    }
+
+    public static Keyword parse(String keyword) {
+        if (keyword == null) {
+            return null;
+        }
+
+        for (Keyword val : Keyword.values()) {
+            if (val.pattern.matcher(keyword).matches()) {
+                return val;
+            }
+        }
+
+        return null;
+    }
+
+    public static boolean isKeyword(String keyword) {
+        return parse(keyword) != null;
+    }
+    
+    public Pattern getPattern() {
+        return this.pattern;
+    }
 }
--- src/main/java/edu/nadn2tetris/tokenizer/TokenType.java
+++ src/main/java/edu/nadn2tetris/tokenizer/TokenType.java
@@ -1,9 +1,41 @@
 package edu.nadn2tetris.tokenizer;
 
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum TokenType {
-    KEYWORD,
-    SYMBOL,
-    IDENTIFIER,
-    INT_CONST,  // [0, 32767] by spec
-    STRING_CONST
+    KEYWORD(createKeywordPatten()),
+    //    SYMBOL(Pattern.compile()),
+    IDENTIFIER(Pattern.compile("^[_a-zA-Z]+[_a-zA-Z\\d]*$")),
+    INT_CONST(Pattern.compile("^\\d{1,5}$")),  // [0, 32767] by spec
+    STRING_CONST(Pattern.compile("^\".*\"$"));
+
+    private final Pattern pattern;
+
+    TokenType(Pattern pattern) {
+        this.pattern = pattern;
+    }
+
+    public static TokenType parse(String value) {
+        if (value == null) {
+            return null;
+        }
+
+        for (TokenType val : TokenType.values()) {
+            if (val.pattern.matcher(value).matches()) {
+                return val;
+            }
+        }
+
+        return null;
+    }
+
+    private static Pattern createKeywordPatten() {
+        final String keywordsUnion = Stream.of(Keyword.values())
+                .map(p -> p.getPattern().pattern().substring(1, p.getPattern().pattern().length() - 1))
+                .collect(Collectors.joining("|"));
+
+        return Pattern.compile("^(" + keywordsUnion + ")$");
+    }
 }
