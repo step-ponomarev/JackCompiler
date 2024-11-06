a4cc1d1f5ea5dffaee7daa9236cf11c39ad96dcc
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2024-11-07 02:30:32 +0300

    row tokenizer -> static

--- src/main/java/edu/nadn2tetris/tokenizer/JackTokenizer.java
+++ src/main/java/edu/nadn2tetris/tokenizer/JackTokenizer.java
@@ -14,6 +14,7 @@ import edu.nadn2tetris.common.Keyword;
 import edu.nadn2tetris.common.TokenType;
 
 public final class JackTokenizer implements Closeable {
+    private static final RowTokenizer ROW_TOKENIZER = new RowTokenizer();
     private final BufferedReader reader;
     private TokenType tokenType;
     private Keyword keyword;
@@ -28,7 +29,7 @@ public final class JackTokenizer implements Closeable {
 
         final Stream<String> lines = this.reader.lines();
         this.tokens = StreamSupport.stream(new RowFilterSpliterator(lines.spliterator()), false)
-                .flatMap(new RowTokenizer())
+                .flatMap(ROW_TOKENIZER)
                 .filter(Objects::nonNull)
                 .iterator();
     }
