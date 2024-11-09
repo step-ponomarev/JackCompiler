6e545dcdba6d35c04a8656fad86c664ee4253157
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2024-11-09 17:38:57 +0300

    added todo

--- src/main/java/edu/nadn2tetris/compiler/CompilationEngine.java
+++ src/main/java/edu/nadn2tetris/compiler/CompilationEngine.java
@@ -13,9 +13,10 @@ import edu.nadn2tetris.tokenizer.JackTokenizer;
 // странцица 310 10.1.3 Синтаксический раздел
 // Читаем первый символ от Tokenizer(a), решаем что за конструкция перед нами - рендерим конструкцию.
 //TODO: 1) Инкапсулировать xml запсь(чтобы не дублировать wrap) +
-//      2) Писать сразу в буффер
+//      2) Писать сразу в буффер или в один XML и переиспользовать публичные методы.
 //      3) Рекурсия(?)
 //      4) <class> <subroutineDec> <parameterList> <subroutineBody> <varDec> <statements>  <letStatement> <expression> <term> <expressionList>...
+//      5)
 public final class CompilationEngine implements Closeable {
     private static final String TAB = "\t";
     private final JackTokenizer tokenizer;
